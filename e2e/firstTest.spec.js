8f82846dd2c2f4e07fba2d65ddb2148769a97c00
Thien Tran <thien.tran@codecomplete.jp>
2018-11-28 17:59:01 +0700

    new tc

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -8,16 +8,12 @@ describe('Example', () => {
   });
 
   it('should show next screen after tap', async () => {
-    await element(by.text('Next')).tap();
-    await expect(element(by.text('Next screen'))).toBeVisible();
+    await element(by.text('Next 2nd screen')).tap();
+    await expect(element(by.text('2nd screen'))).toBeVisible();
   });
-  // it('should show hello screen after tap', async () => {
-  //   await element(by.id('hello_button')).tap();
-  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
-  // });
 
-  // it('should show world screen after tap', async () => {
-  //   await element(by.id('world_button')).tap();
-  //   await expect(element(by.text('World!!!'))).toBeVisible();
-  // });
+  it('should show next screen after tap', async () => {
+    await element(by.text('Next 3rd screen')).tap();
+    await expect(element(by.text('3rd screen'))).toBeVisible();
+  });
 })
